f56ff23f54d17feba9ec79cd812a7f04857331ef
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-11-22 11:33:57 +0100

    ♻️ Search refacto

---
+++ src/components/searchEngine/Main.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Logo from './logo/Logo.svelte';
+    import Search from '../Search.svelte';
+</script>
+
+<div class="app">
+    <Logo/>
+    <Search/>
+</div>
+
+<style>
+    .app {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,13 @@
 <script>
-    import { onMount } from 'svelte';
-    import Logo from '../components/logo/Logo.svelte';
-    import Search from '../components/Search.svelte';
+  import { onMount } from "svelte";
+  import SearchEngine from "../components/searchEngine/Main.svelte";
 
-    let data;
-    onMount(async () => {
-        const response = await fetch('/api/github');
-        data = await response.json();
-        console.log(data)
-    });
+  let data;
+  onMount(async () => {
+    const response = await fetch("/api/github");
+    data = await response.json();
+    console.log(data);
+  });
 </script>
 
-<div class="app">
-    <Logo/>
-    <Search/>
-</div>
-
-<style>
-    .app {
-        align-items: center;
-        display: flex;
-        flex-direction: column;
-    }
-</style>
+<SearchEngine />
